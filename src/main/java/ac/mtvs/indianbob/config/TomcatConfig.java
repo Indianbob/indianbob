c92b84278af83f0377c1e6eeff811469701a12c9
DonghwooCho <whehdgn1001@kookmin.ac.kr>
2022-09-29 13:38:06 +0900

    Add push를 위한 commit

---
+++ src/main/java/ac/mtvs/indianbob/config/TomcatConfig.java
@@ -0,0 +1,16 @@
+package ac.mtvs.indianbob.config;
+
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.stereotype.Component;
+
+import java.io.File;
+
+@Component
+public class TomcatConfig implements WebServerFactoryCustomizer<TomcatServletWebServerFactory> {
+
+    @Override
+    public void customize(TomcatServletWebServerFactory factory) {
+        factory.setDocumentRoot(new File("D:/indianbob/indianbob/src/main/webapp/resources/static/"));
+    }
+}
